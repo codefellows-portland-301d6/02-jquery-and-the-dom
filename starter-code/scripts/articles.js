3dbe57138d288a6ed4a2ec42ec768448c33c7345
Nathan Hugon <nthugon@gmail.com>
2016-09-20 13:47:26 -0700

    first set of todos todone

--- starter-code/scripts/articles.js
+++ starter-code/scripts/articles.js
@@ -3,7 +3,12 @@ var articles = [];
 function Article (opts) {
   // TODO: Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
@@ -18,6 +23,8 @@ Article.prototype.toHtml = function() {
     3. article title
     4. article body, and
     5. publication date. */
+  $newArticle.find('h1').text(this.title);
+
 
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
@@ -26,6 +33,8 @@ Article.prototype.toHtml = function() {
   as it now has real data attached to it! We need to account
   for that before this current article gets rendered to our
   DOM. */
+
+  $newArticle.removeClass('template');
 
   return $newArticle;
 };
