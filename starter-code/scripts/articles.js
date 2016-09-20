fd9669df7ef6a3aeacdc891ce9f0631875ecee0a
dratech <alfonsotech@nym.hush.com>
2016-09-20 14:35:50 -0700

    Build in template clone with properties

--- starter-code/scripts/articles.js
+++ starter-code/scripts/articles.js
@@ -10,11 +10,11 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
   this.body = opts.body;
 }
-
+// Building the article
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-
+  // $newArticle.attr('data-author', this.author);
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
@@ -23,7 +23,10 @@ Article.prototype.toHtml = function() {
     3. article title
     4. article body, and
     5. publication date. */
-
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').css('href', this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.article-body').html(this.body);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
@@ -42,7 +45,7 @@ ourLocalData.sort(function(curElem, nextElem) {
 ourLocalData.forEach(function(ele) {
   articles.push(new Article(ele));
 });
-
+//Rendering to HTML
 articles.forEach(function(a) {
   $('#articles').append(a.toHtml());
 });
