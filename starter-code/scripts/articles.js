cbfa32a7f93c8ae28ee40c4bbe3a12ebfaeda73d
Andrew Beers <andybeers@gmail.com>
2016-09-20 14:28:40 -0700

    Removed class of .template from Article instance to display to the page.

--- starter-code/scripts/articles.js
+++ starter-code/scripts/articles.js
@@ -45,6 +45,8 @@ Article.prototype.toHtml = function() {
   for that before this current article gets rendered to our
   DOM. */
 
+  $newArticle.removeClass("template");
+
   return $newArticle;
 };
 
