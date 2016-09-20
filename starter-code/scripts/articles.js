b1d0ef2c86dd37aa11b1b924e83724773345a10d
Andrew Beers <andybeers@gmail.com>
2016-09-20 14:35:36 -0700

    Changed body from text to html

--- starter-code/scripts/articles.js
+++ starter-code/scripts/articles.js
@@ -35,7 +35,7 @@ Article.prototype.toHtml = function() {
 
   $newArticle.find('header h1').text(this.title);
 
-  $newArticle.find('.article-body').text(this.body);
+  $newArticle.find('.article-body').html(this.body);
 
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
