1dc80c405041db8424937fe2f55a1ea3872a6ec3
Tyler Smith <tyler.smith6891@gmail.com>
2016-09-20 13:19:08 -0700

    completed three TODOs

--- starter-code/scripts/articles.js
+++ starter-code/scripts/articles.js
@@ -1,14 +1,26 @@
+'use strict';
+
 var articles = [];
 
 function Article (opts) {
   // TODO: Use the object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`.
+  this.title = opts.title;
+  this.category = opts.category;
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
+  $newArticle.attr('data-author', this.author);
+  $newArticle.attr('data-authorUrl', this.authorUrl);
+  $newArticle.attr('data-title', this.title);
+  $newArticle.attr('data-body', this.body);
+  $newArticle.attr('data-publishedOn', this.publishedOn);
 
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
@@ -18,6 +30,7 @@ Article.prototype.toHtml = function() {
     3. article title
     4. article body, and
     5. publication date. */
+
 
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
